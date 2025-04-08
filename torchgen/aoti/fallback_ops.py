084e554c1c8ae824bc5441431685dad377e21eaa
Bin Bao <binbao@meta.com>
2025-04-08 09:14:11 -0700

    Add aten._weight_int4pack_mm.default to AOTI C shim

--- torchgen/aoti/fallback_ops.py
+++ torchgen/aoti/fallback_ops.py
@@ -31,7 +31,7 @@ inductor_fallback_ops = {
     "aten._flash_attention_forward.default",
     "aten._fused_moving_avg_obs_fq_helper_functional.default",
     "aten._fused_moving_avg_obs_fq_helper.default",
-    "aten._histogramdd_from_bin_cts.default",
+    "aten._histogramdd_from_bin_cts.default",   
     "aten._int_mm.out",
     "aten._pdist_backward.default",
     "aten._pdist_forward.default",
