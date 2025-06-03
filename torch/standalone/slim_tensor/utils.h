be1daae1ccf13a3c2db2e85654ebbc48a995deca
Bin Bao <binbao@meta.com>
2025-06-03 08:08:18 -0700

    [AOTI] Introduce a SlimTensor representation

---
+++ torch/standalone/slim_tensor/utils.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <cstdint>
+#include <stdexcept>
+
+// OK to use c10 headers here because their corresponding cpp files will be
+// included in the final binary.
+#include <torch/standalone/core/Device.h>
+#include <torch/standalone/core/ScalarType.h>
+#include <torch/standalone/slim_tensor/array_ref.h>
+
+namespace torch::standalone {
+inline size_t compute_numel(const ArrayRef& sizes) {
+  int64_t numel = 1;
+  for (auto& s : sizes) {
+    numel *= s;
+  }
+  return numel;
+}
+
+inline size_t compute_nbytes(const ArrayRef& sizes, c10::ScalarType dtype) {
+  return compute_numel(sizes) * c10::elementSize(dtype);
+}
+
+inline size_t compute_nbytes(size_t numel, c10::ScalarType dtype) {
+  return numel * c10::elementSize(dtype);
+}
+} // namespace torch::standalone
