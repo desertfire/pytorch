250920bf028fe1ff4b1eb5a6efd5a5db6378e72c
Serhat Gundem <tatusamell@gmail.com>
2025-06-17 14:11:53 -0700

    lint test_slim_tensor.cpp

--- test/cpp/aoti_standalone/test_slim_tensor.cpp
+++ test/cpp/aoti_standalone/test_slim_tensor.cpp
@@ -2,82 +2,76 @@
 #include <gtest/gtest.h>
 
 #include <c10/core/MemoryFormat.h>
-#include <torch/standalone/slim_tensor/slim_tensor.h>
-#include <torch/standalone/slim_tensor/array_ref.h>
 #include <torch/csrc/inductor/aoti_standalone/factory.h>
+#include <torch/standalone/slim_tensor/array_ref.h>
+#include <torch/standalone/slim_tensor/slim_tensor.h>
 
 namespace torch::standalone {
 
 using ::testing::ElementsAreArray;
 
 TEST(SlimTensorInternalTest, SetSizesContiguous) {
-    SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
-    EXPECT_EQ(tensor.numel(), 1);
+  SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
+  EXPECT_EQ(tensor.numel(), 1);
 
-    std::vector<int64_t> new_size_vec = {2, 3, 4};
-    ArrayRef new_sizes(new_size_vec.data(), new_size_vec.size());
-    tensor.set_sizes_contiguous(new_sizes);
+  std::vector<int64_t> new_size_vec = {2, 3, 4};
+  ArrayRef new_sizes(new_size_vec.data(), new_size_vec.size());
+  tensor.set_sizes_contiguous(new_sizes);
 
-    //
-    EXPECT_EQ(tensor.dim(), 3);
-    EXPECT_EQ(tensor.numel(), 24);
+  EXPECT_EQ(tensor.dim(), 3);
+  EXPECT_EQ(tensor.numel(), 24);
 
-    EXPECT_THAT(tensor.sizes(), ElementsAreArray({2, 3, 4}));
-    EXPECT_THAT(tensor.strides(), ElementsAreArray({12, 4, 1}));
-    EXPECT_TRUE(tensor.is_contiguous());
-
+  EXPECT_THAT(tensor.sizes(), ElementsAreArray({2, 3, 4}));
+  EXPECT_THAT(tensor.strides(), ElementsAreArray({12, 4, 1}));
+  EXPECT_TRUE(tensor.is_contiguous());
 }
 
-
 TEST(SlimTensorInternalTest, SetSizesAndStridesNonContiguous) {
+  SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
 
-    SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
+  // Set sizes and strides to represent a transposed tensor.
+  // This is equivalent to a (2, 4) tensor that has been transposed to (4, 2).
+  std::vector<int64_t> new_size_vec = {4, 2};
 
-    // Set sizes and strides to represent a transposed tensor.
-    // This is equivalent to a (2, 4) tensor that has been transposed to (4, 2).
-    std::vector<int64_t> new_size_vec = {4, 2};
+  // Strides of original (2, 4) were {4, 1}
+  std::vector<int64_t> new_stride_vec = {1, 4};
+  ArrayRef new_sizes(new_size_vec.data(), new_size_vec.size());
+  ArrayRef new_strides(new_stride_vec.data(), new_stride_vec.size());
 
-    // Strides of original (2, 4) were {4, 1}
-    std::vector<int64_t> new_stride_vec = {1, 4};
-    ArrayRef new_sizes(new_size_vec.data(), new_size_vec.size());
-    ArrayRef new_strides(new_stride_vec.data(), new_stride_vec.size());
+  tensor.set_sizes_and_strides(new_sizes, new_strides);
 
-    tensor.set_sizes_and_strides(new_sizes, new_strides);
+  // verify
+  EXPECT_EQ(tensor.dim(), 2);
+  EXPECT_EQ(tensor.numel(), 8);
+  EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
+  EXPECT_THAT(tensor.strides(), ElementsAreArray({1, 4}));
 
-    // verify
-    EXPECT_EQ(tensor.dim(), 2);
-    EXPECT_EQ(tensor.numel(), 8);
-    EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
-    EXPECT_THAT(tensor.strides(), ElementsAreArray({1, 4}));
-
-    // A transposed tensor is not contiguous.
-    EXPECT_FALSE(tensor.is_contiguous());
+  // A transposed tensor is not contiguous.
+  EXPECT_FALSE(tensor.is_contiguous());
 }
 
 TEST(SlimTensorInternalTest, EmptyTensorRestride) {
-    SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
-    std::vector<int64_t> size_vec = {4, 2};
-    std::vector<int64_t> stride_vec = {1, 4}; // Non-contiguous strides
-    tensor.set_sizes_and_strides(
-        ArrayRef(size_vec.data(), size_vec.size()),
-        ArrayRef(stride_vec.data(), stride_vec.size())
-    );
-    // it shouldn't be contiguous first
-    EXPECT_FALSE(tensor.is_contiguous());
+  SlimTensor tensor = create_empty_tensor({}, {}, c10::kFloat);
+  std::vector<int64_t> size_vec = {4, 2};
+  std::vector<int64_t> stride_vec = {1, 4}; // Non-contiguous strides
+  tensor.set_sizes_and_strides(
+      ArrayRef(size_vec.data(), size_vec.size()),
+      ArrayRef(stride_vec.data(), stride_vec.size()));
+  // it shouldn't be contiguous first
+  EXPECT_FALSE(tensor.is_contiguous());
 
-    // make the tensor contiguous.
-    tensor.empty_tensor_restride(c10::MemoryFormat::Contiguous);
+  // make the tensor contiguous.
+  tensor.empty_tensor_restride(c10::MemoryFormat::Contiguous);
 
-    // Sizes should NOT have changed.
-    EXPECT_EQ(tensor.dim(), 2);
-    EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
+  // Sizes should NOT have changed.
+  EXPECT_EQ(tensor.dim(), 2);
+  EXPECT_THAT(tensor.sizes(), ElementsAreArray({4, 2}));
 
-    // Strides SHOULD have changed to be contiguous for a (4, 2) tensor.
-    EXPECT_THAT(tensor.strides(), ElementsAreArray({2, 1}));
+  // Strides SHOULD have changed to be contiguous for a (4, 2) tensor.
+  EXPECT_THAT(tensor.strides(), ElementsAreArray({2, 1}));
 
-    // The contiguity flag should now be true.
-    EXPECT_TRUE(tensor.is_contiguous());
-
+  // The contiguity flag should now be true.
+  EXPECT_TRUE(tensor.is_contiguous());
 }
 
 } // namespace torch::standalone
