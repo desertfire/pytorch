cc6a30a1da722d19db660489c73b301aad36d051
Serhat Gundem <tatusamell@gmail.com>
2025-06-18 14:11:38 -0700

    remove middleware resize

--- torch/csrc/inductor/aoti_standalone/cpu/resize.h
+++ torch/csrc/inductor/aoti_standalone/cpu/resize.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <torch/csrc/inductor/aoti_standalone/c/shim.h>
-#include <torch/csrc/inductor/aoti_standalone/resize.h>
+#include <torch/standalone/resize_template.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -15,7 +15,13 @@ AOTITorchError aoti_torch_cpu_resize_(
     int64_t size_len_,
     int32_t* memory_format) {
   try {
-    resize_(self, size, size_len_, memory_format);
+    SlimTensor* tensor = reinterpret_cast<SlimTensor*>(self);
+    ArrayRef size_ref(size, size_len_);
+    std::optional<c10::MemoryFormat> optional_memory_format;
+    if (memory_format) {
+      optional_memory_format = static_cast<c10::MemoryFormat>(*memory_format);
+    }
+    _resize_(*tensor, size_ref, optional_memory_format);
     return AOTI_TORCH_SUCCESS;
   } catch (const std::exception& e) {
     return AOTI_TORCH_FAILURE;
